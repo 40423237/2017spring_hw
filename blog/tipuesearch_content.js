b96d713036c1cb3157fe4a949299587e0dffa12e
40423237 <40423237@gm.nfu.edu.tw>
2017-04-12 10:55:36 +0800

    week1

--- blog/tipuesearch_content.js
+++ blog/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages":[{"url":"./pages/about/","title":"About","tags":"misc","text":"2017Spring 機械設計工程系協同產品設計實習 40423237陳憲棠課程倉儲 40423237陳憲棠課程投影片 40423237陳憲棠課程網誌"},{"url":"./week1.html","title":"20170222 week1","tags":"Course","text":""}]};
+var tipuesearch = {"pages":[{"tags":"misc","title":"About","url":"./pages/about/","text":"2017Spring 機械設計工程系協同產品設計實習 40423237陳憲棠課程倉儲 40423237陳憲棠課程投影片 40423237陳憲棠課程網誌"},{"tags":"Course","title":"20170222 week1","url":"./week1.html","text":""}]};
